9777c94dcda13353d704045dd99b2c52211bba00
Elliot Hill <elliothill1407@gmail.com>
2024-05-23 15:56:29 +1000

    comment connect-go client

--- connect-go-demo/cmd/client/main.go
+++ connect-go-demo/cmd/client/main.go
@@ -3,27 +3,32 @@ package main
 import (
 	"context"
 	"log"
-	"net/http"
+	"net/http" // provides HTTP client and server implementations
 
+	"connectrpc.com/connect" // import the Connect package
+
+	// import the generated gRPC code for the Greet service (by protoc-gen-go)
 	greetv1 "connect-go-and-grpc/connect-go-demo/gen/greet/v1"
-	"connect-go-and-grpc/connect-go-demo/gen/greet/v1/greetv1connect"
 
-	"connectrpc.com/connect"
+	// import the generated Connect-Go code for the Greet service (by protoc-gen-connect-go)
+	"connect-go-and-grpc/connect-go-demo/gen/greet/v1/greetv1connect"
 )
 
 func main() {
+	// create a new Greet service client
 	client := greetv1connect.NewGreetServiceClient(
-		http.DefaultClient,
+		http.DefaultClient, // use the default HTTP client
 		"http://localhost:8080",
-		// connect.WithGRPC(),
 	)
-	res, err := client.Greet(
-		context.Background(),
-		connect.NewRequest(&greetv1.GreetRequest{Name: "Jane"}),
-	)
+
+	// create a new request with the name "Jane"
+	req := connect.NewRequest(&greetv1.GreetRequest{Name: "Jane"})
+
+	// call the Greet method on the client with a background context and the request
+	res, err := client.Greet(context.Background(), req)
 	if err != nil {
-		log.Println(err)
-		return
+		log.Fatalf("could not greet: %v", err)
 	}
-	log.Println(res.Msg.Greeting)
+
+	log.Printf("Greeting: %s", res.Msg.Greeting)
 }
